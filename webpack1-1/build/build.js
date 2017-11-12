23b86a5a65babfe5300ca240173aef67236193ac
VaJoy Larn <vajoy@qq.com>
2017-11-12 22:54:51 +0800

    1

--- webpack1-1/build/build.js
+++ webpack1-1/build/build.js
@@ -1,62 +1,68 @@
-/******/var vajoy=
-/******/(function(document, undefined) {
-/******/	return function(modules) {
-/******/		var installedModules = {};
-/******/		function require(moduleId) {
-/******/			if(installedModules[moduleId])
-/******/				return installedModules[moduleId];
-/******/			var module = installedModules[moduleId] = {
-/******/				exports: {}
-/******/			};
-/******/			modules[moduleId](module, module.exports, require);
-/******/			return module.exports;
-/******/		}
-/******/		require.ensure = function(chunkId, callback) {
-/******/			callback(require);
-/******/		};
-/******/		return require(0);
-/******/	}
-/******/})(document)
-/******/({
-/******/0: function(module, exports, require) {
+var vajoy =   // 可以在客户端执行 vajoy.
+    (function (document, undefined) {
+        return function (modules) {
+            var installedModules = {};
+            // 定义 require 方法
+            function require(moduleId) {
+                if (installedModules[moduleId])
+                    return installedModules[moduleId].exports;
+                // 客户端是没有 Node 侧的 module/module.exports 的
+                // 需要定义一个，来解析模块代码中的 module/module.exports
+                var module = installedModules[moduleId] = {
+                    exports: {}
+                };
+                modules[moduleId](module, module.exports, require);
+                return module.exports;
+            }
+            // 定义 require.ensure 方法
+            require.ensure = function (chunkId, callback) {
+                callback(require);
+            };
+            return require(0);
+        }
+    })(document)
+    ({
+        0: function (module, exports, require) {
 
-var a = require(1);
-var b = require(2);
-require.ensure(1, function(require) {
-    require(2).xyz();
-    var d = require(4);
-});
+            var a = require(1);
+            var b = require(2);
+            require.ensure(1, function (require) {
+                require(2).xyz();
+                var d = require(4);
+            });
 
-module.exports = {
-    test: function(){console.log(123)}
-};
+            module.exports = {
+                test: function () {
+                    console.log(123)
+                }
+            };
 
-/******/},
-/******/
-/******/1: function(module, exports, require) {
+        },
 
-console.log('a');
+        1: function (module, exports, require) {
 
-/******/},
-/******/
-/******/2: function(module, exports, require) {
+            console.log('a');
 
-exports.xyz = function(){
-    console.log('b module output')
-};
+        },
 
-/******/},
-/******/
-/******/3: function(module, exports, require) {
+        2: function (module, exports, require) {
 
-console.log('c');
+            exports.xyz = function () {
+                console.log('b module output')
+            };
 
-/******/},
-/******/
-/******/4: function(module, exports, require) {
+        },
 
-console.log('d');
+        3: function (module, exports, require) {
 
-/******/},
-/******/
-/******/})
+            console.log('c');
+
+        },
+
+        4: function (module, exports, require) {
+
+            console.log('d');
+
+        },
+
+    })
